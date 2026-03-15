bbe7a724f2b06cd2be5dc61c578fac3ae6b9fd38
L V Ramana <lellalvramana@gmail.com>
2026-03-15 13:48:02 +0530

    shared-lib

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -4,7 +4,7 @@ def call(Map configMap){
             agent{ label 'agent-1'}
             
             environment{
-                greeting=configMap.get('greeting')
+                greeting = configMap.get('greeting')
             }
         }
 
@@ -18,22 +18,3 @@ def call(Map configMap){
         }
 }
 
-def call(Map condfigMap){
-
-    pipeline {
-        agent { label 'agent-1' }
-
-        environment {
-            greeting = condfigMap.get('greeting')
-        }
-
-        stages {
-            stage('printing-greeting') {
-                steps {
-                    echo "${greeting}"
-                }
-            }
-        }
-    }
-
-}
