279a39c67ad6c16008c5c96ba7ad331ef8932d20
L V Ramana <lellalvramana@gmail.com>
2026-03-15 13:17:08 +0530

    shared-lib

---
+++ samplePipeline.groovy
@@ -0,0 +1,16 @@
+def call(Map condfigMap){
+
+        pipeline{
+            agent{ label 'agent-1'}
+            
+            environment{
+                greeting: condfigMap.get('greeting')
+            }
+        }
+
+        stages{
+            stage('printing-greeting'){
+                echo '${greeting}'
+            }
+        }
+}
